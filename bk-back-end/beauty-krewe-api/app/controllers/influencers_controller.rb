081f77d2a2b1074ba04aee707b1542d7b9fb6ca1
jeanettepayne <jeanetterpayne@gmail.com>
2021-08-25 22:06:13 -0400

    added index and create to controller

--- bk-back-end/beauty-krewe-api/app/controllers/influencers_controller.rb
+++ bk-back-end/beauty-krewe-api/app/controllers/influencers_controller.rb
@@ -1,2 +1,17 @@
 class InfluencersController < ApplicationController
+
+    def index
+        influencers = Influencer.all
+        render json: Influencer.new(inflencers)
+    end
+
+    def create
+        influencer = Influencer.new(name: params[:name])
+
+        if influencer.save
+            render json: InfluencerSerializer.new(influencer), status: :accepted
+        else
+            render json: {errors: board.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
 end
