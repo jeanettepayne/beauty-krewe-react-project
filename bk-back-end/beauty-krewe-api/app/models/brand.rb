0e3740a6fc26d056de8590b00f2665d44649202f
jeanettepayne <jeanetterpayne@gmail.com>
2021-08-25 20:21:54 -0400

    added associations

---
+++ bk-back-end/beauty-krewe-api/app/models/brand.rb
@@ -0,0 +1,4 @@
+class Brand < ActiveRecord::Base
+    has_many :products
+    
+end
---
+++ bk-back-end/beauty-krewe-api/app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ActiveRecord::Base
+    belongs_to :brand
+    has_and_belongs_to_many :influencers
+    
+end
