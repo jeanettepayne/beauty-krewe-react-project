2163b0058093b593422b7cb072affefa8e9f029c
jeanettepayne <jeanetterpayne@gmail.com>
2021-08-28 15:09:50 -0400

    added logic to methods

--- bk-back-end/beauty-krewe-api/app/controllers/api/products_controller.rb
+++ bk-back-end/beauty-krewe-api/app/controllers/api/products_controller.rb
@@ -1,19 +1,27 @@
 class Api::ProductsController < ApplicationController
 
     def index
-
+        products = Product.all
+        render json: Product.new(products)
     end
 
     def create
-
+        product = Product.new(product_params)
+        if product.save
+            render json: Product.new(product)
+        else
+            render json: {error: 'Oops! There was an error adding the product'}
+        end
     end
 
     def show
-
+        product = Product.find(params[:id])
+        render json: Product.new(product)
     end
 
     def destroy
-
+        product = Product.find(params[:id])
+        product.destroy
     end
 
     private
