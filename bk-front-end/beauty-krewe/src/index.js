f90677e873642785be85c7729766e320d78b89e0
jeanettepayne <jeanetterpayne@gmail.com>
2021-08-28 13:00:55 -0400

    added Footer component

--- bk-front-end/beauty-krewe/src/index.js
+++ bk-front-end/beauty-krewe/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './Header';
+import Footer from './Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     <Header />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
