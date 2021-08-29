71b1ffbe62b9932c13a568af0e77f3ae911d552d
jeanettepayne <jeanetterpayne@gmail.com>
2021-08-28 21:40:08 -0400

    added redux dependencies

--- bk-front-end/beauty-krewe/src/App.js
+++ bk-front-end/beauty-krewe/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 class App extends React.Component {
 
   componentDidMount() {
-    fetch('http://localhost:3001/api')
+    fetch('http://localhost:3001/api/products')
     .then(response => response.json())
     .then(data => console.log(data))
   }
