19e8ad1d8d73a541670e3e9db70d9beeed90f83a
jeanettepayne <jeanetterpayne@gmail.com>
2021-08-30 21:01:01 -0400

    added default value and onChange

--- bk-front-end/beauty-krewe/src/components/ProductForm.js
+++ bk-front-end/beauty-krewe/src/components/ProductForm.js
@@ -3,22 +3,33 @@ import React from 'react';
 
 class ProductForm extends React.Component {
 
+    state = {
+        name: '', 
+        price: '', 
+        rating: '', 
+        description: ''
+    }
+
+    handleChange = (event) => {
+
+    }
+
     render() {
         return (
             <div>
                 This is where the product form goes.
                 <form>
                     <label>Product Name: </label>
-                    <input type="text" placeholder="Enter Name"/>
+                    <input type="text" placeholder="Enter Name" value={this.state.name} onChange={this.handleChange}/>
                     <br></br>
                     <label>Product Price: </label>
-                    <input type="text" placeholder="Enter Price (without $)"/>
+                    <input type="text" placeholder="Enter Price (without $)" value={this.state.price} onChange={this.handleChange}/>
                     <br></br>
                     <label>Product Rating: </label>
-                    <input type="text" placeholder="Enter Rating"/>
+                    <input type="text" placeholder="Enter Rating" value={this.state.rating} onChange={this.handleChange}/>
                     <br></br>
                     <label>Product Description: </label>
-                    <textarea placeholder="Enter Description"/>
+                    <textarea placeholder="Enter Description" value={this.state.description} onChange={this.handleChange}/>
                     <br></br>
                     <input type="reset" value="Reset Form" />
                     <input type="submit" value="Submit" />
