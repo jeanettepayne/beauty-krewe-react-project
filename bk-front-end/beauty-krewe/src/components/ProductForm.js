472d9004bd2a752f27f643fe674a18fd05a6af8d
jeanettepayne <jeanetterpayne@gmail.com>
2021-08-30 20:48:27 -0400

    added fields and buttons to form

--- bk-front-end/beauty-krewe/src/components/ProductForm.js
+++ bk-front-end/beauty-krewe/src/components/ProductForm.js
@@ -10,6 +10,19 @@ class ProductForm extends React.Component {
                 <form>
                     <label>Product Name: </label>
                     <input type="text" placeholder="Enter Name"/>
+                    <br></br>
+                    <label>Product Price: </label>
+                    <input type="text" placeholder="Enter Price (without $)"/>
+                    <br></br>
+                    <label>Product Rating: </label>
+                    <input type="text" placeholder="Enter Rating"/>
+                    <br></br>
+                    <label>Product Description: </label>
+                    <textarea placeholder="Enter Description"/>
+                    <br></br>
+                    <input type="reset" value="Reset Form" />
+                    <input type="submit" value="Submit" />
+                    
                 </form>
             </div>
         )
