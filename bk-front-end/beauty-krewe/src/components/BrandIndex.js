28e25a5afc7c3b8dd227a0e12ade163c064eb172
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-02 19:54:05 -0400

    added links to brands

--- bk-front-end/beauty-krewe/src/components/BrandIndex.js
+++ bk-front-end/beauty-krewe/src/components/BrandIndex.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Brand from './Brand';
+import { Route, Link } from 'react-router-dom';
 
 const BrandIndex = (props) => {
 
     return(
         <div>
-            {props.brands.map(brand => <div key={brand.id}> <Brand brand={brand} /> </div>)}
+            {props.brands.map(brand => <div key={brand.id}> <Link path={`/brands/${brand.id}`}>{brand.name}</Link>  </div>)}
         </div>
     )
 
 }
 
 export default BrandIndex;
+
+{/* <Brand brand={brand} /> */}
