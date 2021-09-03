2e2c2737984675fbf6e0c1e5a07c99d7935b6a11
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-02 20:26:18 -0400

    fixed link, made brand name h2

--- bk-front-end/beauty-krewe/src/components/Brand.js
+++ bk-front-end/beauty-krewe/src/components/Brand.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 const Brand = (props) => {
-
+    // fix to find brand id, not array item
     let brand = props.brands[props.match.params.id - 1]
 
     return (
-        <li>
+        <h2>
             {brand ? brand.name : null}
-        </li>
+            {/* {brand ? null : <Redirect to='/brands'/>} */}
+        </h2>
     )
 }
 
--- bk-front-end/beauty-krewe/src/components/BrandIndex.js
+++ bk-front-end/beauty-krewe/src/components/BrandIndex.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Brand from './Brand';
+// import Brand from './Brand';
 import { Route, Link } from 'react-router-dom';
 
 const BrandIndex = (props) => {
 
     return(
         <div>
-            {props.brands.map(brand => <div key={brand.id}> <Link path={`/brands/${brand.id}`}>{brand.name}</Link>  </div>)}
+            {props.brands.map(brand => <div key={brand.id}> <Link to={`/brands/${brand.id}`}>{brand.name}</Link>  </div>)}
         </div>
     )
 
