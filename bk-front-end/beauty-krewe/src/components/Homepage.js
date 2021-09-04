5ec6b6c1846d3b8685bdfe85212f96d78719c194
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-04 14:18:25 -0400

    added link to browse by brand

--- bk-front-end/beauty-krewe/src/components/Homepage.js
+++ bk-front-end/beauty-krewe/src/components/Homepage.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
-const Homepage = () => {
+const Homepage = (props) => {
 
     return (
         <div>
             This is the homepage
+            <br></br>
+            <Link to={'/brands'}>Browse by Brand</Link>
+            <br></br>
+            Link to browse by Influencer
         </div>
     )
 }
