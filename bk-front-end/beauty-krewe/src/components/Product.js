bf2c47ed4e085be9eb5e4131001298c24206d4c5
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-05 13:20:27 -0400

    cleanup

--- bk-front-end/beauty-krewe/src/components/Product.js
+++ bk-front-end/beauty-krewe/src/components/Product.js
@@ -1,28 +1,5 @@
 import React from 'react';
 
-// class Product extends React.Component {
-    
-
-//     // let product = props.products.filter(product => product.id == props.match.params.id)[0]
-//     render() {
-//         // let product = props.product.filter(product => product.id == props.match.params.id)[0]
-//         let product = this.props.product
-//         return (
-//             <div>
-//                 {product.name}
-//                 {/* {props.products && props.products.map(product =>  */}
-//                     {/* <div key={this.props.product.id}> */}
-//                         test name
-//                         {/* {product.name} */}
-//                     {/* </div> */}
-//                     {/* } */}
-//             </div>
-//         )
-//     }
-// }
-
-// export default Product;
-
 const Product = (props) => {
 
     console.log(props)
