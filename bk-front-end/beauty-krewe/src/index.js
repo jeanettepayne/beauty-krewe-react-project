18f98ac4c9fdd2fc9616c4da784f9b18227eea26
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-05 11:08:41 -0400

    minor cleanup

--- bk-front-end/beauty-krewe/src/index.js
+++ bk-front-end/beauty-krewe/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Header from './Header';
-// import Footer from './Footer';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -31,8 +29,6 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <App />
-      {/* <Header /> */}
-      {/* <Footer /> */}
     </Router>
   </Provider>,
   document.getElementById('root')
