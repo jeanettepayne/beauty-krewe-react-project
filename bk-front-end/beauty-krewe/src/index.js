d5bae99ee564100bee94570d33cd731fd648b5e8
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-05 11:06:30 -0400

    added combineReducer/rootReducer for all 3 reducers

--- bk-front-end/beauty-krewe/src/index.js
+++ bk-front-end/beauty-krewe/src/index.js
@@ -4,23 +4,28 @@ import './index.css';
 import App from './App';
 // import Header from './Header';
 // import Footer from './Footer';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import brandReducer from './reducers/brandReducer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import influencerReducer from './reducers/influencerReducer';
-// import productReducer from './reducers/productReducer';
-// import rootReducer from './reducers/rootReducer';
+import productReducer from './reducers/productReducer';
 
 
 //store setup, storing data globally
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const rootReducer = combineReducers({
+  brandReducer,
+  productReducer,
+  influencerReducer
+})
+
 // let store = createStore(brandReducer, composeEnhancers(applyMiddleware(thunk)))
-let store = createStore(influencerReducer, composeEnhancers(applyMiddleware(thunk)))
+// let store = createStore(influencerReducer, composeEnhancers(applyMiddleware(thunk)))
 // let store = createStore(productReducer, composeEnhancers(applyMiddleware(thunk)))
-// let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
