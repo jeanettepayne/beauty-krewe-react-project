fdc946f3a095897bfce304a5266ea1965e3596ec
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-06 11:12:09 -0400

    added accordion display

--- bk-front-end/beauty-krewe/src/components/ProductIndex.js
+++ bk-front-end/beauty-krewe/src/components/ProductIndex.js
@@ -1,16 +1,38 @@
 import React from 'react';
 // import Product from './Product';
 import { Route, Link } from 'react-router-dom';
+import Accordion from 'react-bootstrap/Accordion'
 
 const ProductIndex = (props) => {
     console.log(props)
 
     return(
         <div>
+            <h2>
+                All Products
+            </h2>
             {/* {props.products && props.products.map(product => <div key={product.id}> <Product product={product} /> </div>)} */}
-            {/* testing */}
-            {props.products && props.products[0].map(product => <div key={product.id}> <Link to={`/products/${product.id}`}> {product.name} </Link> </div>)}
-            {props.products && props.products[1].map(product => <div key={product.id}> <Link to={`/products/${product.id}`}> {product.name} </Link> </div>)}
+            {/* UNCOMMENT LINE BELOW IF ACCORDION DOES NOT WORK */}
+            {/* {props.products && props.products[0].map(product => <div key={product.id}> <Link to={`/products/${product.id}`}> {product.name} </Link> </div>)} */}
+            {/* {props.products && props.products[1].map(product => <div key={product.id}> <Link to={`/products/${product.id}`}> {product.name} </Link> </div>)} */}
+
+
+            {/* // ACCORDION ATTEMPT */}
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Accordion>
+                {props.products[0] && props.products[0].map(product =>
+                    <Accordion.Item eventKey={product.id}>
+                        <Accordion.Header>{product.name}</Accordion.Header>
+                            <Accordion.Body>
+                                Brand: {product.brand.name}<br></br>
+                                Price: ${product.price}<br></br>
+                                Rating: {product.rating}<br></br>
+                                Description: {product.description}<br></br>
+                            
+                            </Accordion.Body>
+                    </Accordion.Item>)}
+            </Accordion>
+        </div>
             
         </div>
     )
