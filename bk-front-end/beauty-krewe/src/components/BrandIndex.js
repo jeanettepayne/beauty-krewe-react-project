55003df026e76029885b14ae0c64fd801ca80d8f
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-06 10:52:27 -0400

    added button styling

--- bk-front-end/beauty-krewe/src/components/BrandIndex.js
+++ bk-front-end/beauty-krewe/src/components/BrandIndex.js
@@ -1,18 +1,28 @@
 import React from 'react';
 // import Brand from './Brand';
 import { Route, Link } from 'react-router-dom';
-
+import Button from 'react-bootstrap/Button'
 
 const BrandIndex = (props) => {
     console.log(props)
 
     return(
+        // <div>
+        //     <h2>
+        //         Brands
+        //     </h2>
+        //     {props.brands && props.brands[0].map((brand) => <div key={brand.id}> <Link to={`/brands/${brand.id}`}>{brand.name}</Link>  </div>)}
+        //     {/* {props.brands && props.brands[1].map((brand) => <div key={brand.id}> <Link to={`/brands/${brand.id}`}>{brand.name}</Link>  </div>)} */}
+        // </div>
+
         <div>
             <h2>
                 Brands
             </h2>
-            {props.brands && props.brands[0].map((brand) => <div key={brand.id}> <Link to={`/brands/${brand.id}`}>{brand.name}</Link>  </div>)}
-            {/* {props.brands && props.brands[1].map((brand) => <div key={brand.id}> <Link to={`/brands/${brand.id}`}>{brand.name}</Link>  </div>)} */}
+            <div className="d-grid gap-2">
+            {props.brands && props.brands[0].map(brand => 
+                <Link to={`/brands/${brand.id}`}> <Button key={brand.id} variant="secondary">{brand.name}</Button> </Link>)}
+            </div>
         </div>
     )
 
