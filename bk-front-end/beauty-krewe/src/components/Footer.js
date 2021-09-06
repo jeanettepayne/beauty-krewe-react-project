ce4eb0501e8da01ebd28ec47c8cb6e386e979717
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-06 16:23:09 -0400

    fixed footer for real this time

--- bk-front-end/beauty-krewe/src/components/Footer.js
+++ bk-front-end/beauty-krewe/src/components/Footer.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar';
-import { Container } from 'react-bootstrap';
+
 
 const Footer = () => {
     return (
-        // <div className="footer">
-        //     Beauty Krewe©2021
-        // </div>
-
-        <div>
-            <Navbar bg="light" variant="light" sticky="bottom" style={{display: 'flex', justifyContent: 'center'}}>
+        <div className="empty-container">
+        <div className="footer" fixed="bottom">
             Beauty Krewe©2021
-            </Navbar>
+        </div>
         </div>
     );
 }
