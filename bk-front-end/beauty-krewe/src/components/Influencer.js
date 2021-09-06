9502bb24e2e126b266f8974a311ea5fb09d8c58f
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-05 22:02:53 -0400

    working on accordion functionality

--- bk-front-end/beauty-krewe/src/components/Influencer.js
+++ bk-front-end/beauty-krewe/src/components/Influencer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Influencer = (props) => {
     console.log(props)
 
--- bk-front-end/beauty-krewe/src/components/InfluencerIndex.js
+++ bk-front-end/beauty-krewe/src/components/InfluencerIndex.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Accordion from 'react-bootstrap/Accordion'
 
 const InfluencerIndex = (props) => {
 
     return(
+        // <div>
+        //     <h2>
+        //         Influencers
+        //     </h2>
+        //     {props.influencers && props.influencers.map(influencer => 
+        //            <div key={influencer.id}> <Link to={`/influencers/${influencer.id}`}> {influencer.name} </Link></div>)}
+        // </div>
+
         <div>
-            <h2>
-                Influencers
-            </h2>
-            {props.influencers && props.influencers.map(influencer => <div key={influencer.id}> <Link to={`/influencers/${influencer.id}`}> {influencer.name} </Link></div>)}
+            <h2>Influencers</h2>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Accordion>
+                <Accordion.Item>
+                    <Accordion.Header>Test</Accordion.Header>
+                    <Accordion.Body>
+                    Testing 123<br></br>
+                    again this is a test<br></br>
+                    dear jesus why isn't this working<br></br>
+                    </Accordion.Body>
+                </Accordion.Item>
+
+                <Accordion.Item>
+                   {props.influencers && props.influencers.map(influencer => 
+    
+                     <Accordion.Header> {influencer.name} </Accordion.Header>
+                )}
+                </Accordion.Item>
+            </Accordion>
+            </div>
         </div>
     )
 
