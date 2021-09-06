facd1f5f4b8b879cd8f3b89b8c729ee43cdb89c9
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-06 15:07:01 -0400

    fixed seeding to pull from api

--- bk-back-end/beauty-krewe-api/db/seeds.rb
+++ bk-back-end/beauty-krewe-api/db/seeds.rb
@@ -18,21 +18,40 @@
 # ])
 
 
-    tati = Influencer.create(name: 'Tati Westbrook')
-    kristi = Influencer.create(name: 'RawBeautyKristi')
-    nail = Influencer.create(name: 'SimplyNailogical')
-    jaclyn = Influencer.create(name: 'Jaclyn Hill')
+    # tati = Influencer.create(name: 'Tati Westbrook')
+    # kristi = Influencer.create(name: 'RawBeautyKristi')
+    # nail = Influencer.create(name: 'SimplyNailogical')
+    # jaclyn = Influencer.create(name: 'Jaclyn Hill')
     
-    he = Brand.create(name: 'High End Brand')
-    ab = Brand.create(name: 'Affordable Brand')
+    # he = Brand.create(name: 'High End Brand')
+    # ab = Brand.create(name: 'Affordable Brand')
    
     
-    f1 = Product.create(name: 'Fake Product 1', price: '2.99', rating: '3.5', description: 'fake description 1', brand_id: 2)
-    f2 = Product.create(name: 'Fake Product 2', price: '14.95', rating: '5', description: 'fake description 2', brand_id: 1)
-    f3 = Product.create(name: 'Fake Product 3', price: '7.50', rating: '2.5', description: 'fake description 3', brand_id: 2)
-    f4 = Product.create(name: 'Fake Product 4', price: '27.50', rating: '4.5', description: 'fake description 4', brand_id: 1)
+    # f1 = Product.create(name: 'Fake Product 1', price: '2.99', rating: '3.5', description: 'fake description 1', brand_id: 2)
+    # f2 = Product.create(name: 'Fake Product 2', price: '14.95', rating: '5', description: 'fake description 2', brand_id: 1)
+    # f3 = Product.create(name: 'Fake Product 3', price: '7.50', rating: '2.5', description: 'fake description 3', brand_id: 2)
+    # f4 = Product.create(name: 'Fake Product 4', price: '27.50', rating: '4.5', description: 'fake description 4', brand_id: 1)
 
-    t.products << [f1, f2, f3]
-    kristi.products << [f4, f2]
-    nail.products << [f1, f4, f2]
-    jaclyn.products << [f1, f2, f3, f4]
+    # t.products << [f1, f2, f3]
+    # kristi.products << [f4, f2]
+    # nail.products << [f1, f4, f2]
+    # jaclyn.products << [f1, f2, f3, f4]
+
+    require 'rest-client'
+
+
+        response = RestClient.get 'http://makeup-api.herokuapp.com/api/v1/products.json'
+        json = JSON.parse response
+
+
+            json.map do |product|
+                # newBrand = Brand.create(name: "#{product["brand"]}")
+                newBrand = Brand.find_or_create_by(name: "#{product["brand"]}")
+                Product.create(
+                    name: "#{product["name"]}", 
+                    price: "#{product["price"]}", 
+                    rating: "#{product["rating"]}", 
+                    description: "#{product["description"]}", 
+                    brand: newBrand)
+            end
+    
