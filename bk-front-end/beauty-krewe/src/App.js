763ca961bd3d0f83fdf6a4db5d55c8b03218a595
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-08 14:55:24 -0400

    cleaned up unused imports

--- bk-front-end/beauty-krewe/src/App.js
+++ bk-front-end/beauty-krewe/src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 import React from 'react';
 import { connect } from 'react-redux';
-import fetchBrands from './actions/fetchBrands';
 import BrandsContainer from './containers/BrandsContainer';
 import InfluencersContainer from './containers/InfluencersContainer';
-import BrandIndex from './components/BrandIndex';
-// import Product from './components/Product';
 import { Route } from 'react-router';
 import ProductsContainer from './containers/ProductsContainer';
 import Homepage from './components/Homepage';
-import fetchApi from './actions/fetchApi';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import fetchInfluencersProducts from './actions/fetchInfluencersProducts';
 import InfluencersProductsContainer from './containers/InfluencersProductsContainer';
 
 
