02fcf504ed7276c156549db86b8a56a7eea09802
jeanettepayne <jeanetterpayne@gmail.com>
2021-09-07 20:15:18 -0400

    added ip routes

--- bk-back-end/beauty-krewe-api/config/routes.rb
+++ bk-back-end/beauty-krewe-api/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :influencers
     resources :products
     resources :brands
+    resources :influencers_product
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
